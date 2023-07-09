c41bb9d060735b22702043340048f87c156ba35e
小恸恸 <wtt150249@gmail.com>
2023-07-09 21:03:42 +0800

    feat: 修改 button 的 demo 显示

--- demo/button/example.js
+++ demo/button/example.js
@@ -1,41 +1,69 @@
 import "./load.js";
 
-const line = document.getElementById("lineBtn");
-const setMaxWidth = document.getElementById("setMaxWidth");
-const maxWidthValue = document.getElementById("maxWidthValue");
+const operate = document.getElementById("operate");
 
-setMaxWidth.addEventListener("click", () => {
-	const value = maxWidthValue.value;
-	line.style.maxWidth = value + "px";
+const content = document.getElementById("content");
+document.getElementById("getContent").addEventListener("click", () => {
+	content.value = operate.textContent;
+});
+document.getElementById("setContent").addEventListener("click", () => {
+	operate.textContent = content.value;
 });
 
-const lineValue = document.getElementById("lineValue");
-const setline = document.getElementById("setline");
-
-setline.addEventListener("click", () => {
-	const value = lineValue.value;
-	line.setAttribute("line", value);
+document.getElementById("setMaxWidth").addEventListener("click", () => {
+	const value = document.getElementById("maxWidth").value;
+	operate.style.maxWidth = value + "px";
 });
 
-const lineContent = document.getElementById("lineContent");
-const setLineContent = document.getElementById("setLineContent");
-
-setLineContent.addEventListener("click", () => {
-	const value = lineContent.value;
-	line.textContent = value;
+document.getElementById("setline").addEventListener("click", () => {
+	const value = document.getElementById("lineValue").value;
+	operate.setAttribute("line", value);
 });
 
-const setIcon = document.getElementById("setIcon");
-const removeIcon = document.getElementById("removeIcon");
-
-setIcon.addEventListener("click", () => {
+document.getElementById("setIcon").addEventListener("click", () => {
 	const icon = document.createElement("xtt-icon");
 	icon.setAttribute("icon", "power");
 	icon.slot = "icon";
-	line.appendChild(icon);
+	operate.appendChild(icon);
 });
 
-removeIcon.addEventListener("click", () => {
-	const icon = line.querySelector("xtt-icon");
+document.getElementById("removeIcon").addEventListener("click", () => {
+	const icon = operate.querySelector("xtt-icon");
 	icon?.remove();
+});
+
+document.getElementById("setPrimary").addEventListener("click", () => {
+	operate.setAttribute("type", "primary");
+});
+
+document.getElementById("setBase").addEventListener("click", () => {
+	operate.setAttribute("type", "base");
+});
+
+document.getElementById("removeType").addEventListener("click", () => {
+	operate.removeAttribute("type");
+});
+
+document.getElementById("setText").addEventListener("click", () => {
+	operate.toggleAttribute("text", true);
+});
+
+document.getElementById("removeText").addEventListener("click", () => {
+	operate.removeAttribute("text");
+});
+
+document.getElementById("setLargeSize").addEventListener("click", () => {
+	operate.setAttribute("size", "large");
+});
+
+document.getElementById("removeSize").addEventListener("click", () => {
+	operate.removeAttribute("size");
+});
+
+document.getElementById("setBlock").addEventListener("click", () => {
+	operate.toggleAttribute("block", true);
+});
+
+document.getElementById("removeBlock").addEventListener("click", () => {
+	operate.removeAttribute("block");
 });
