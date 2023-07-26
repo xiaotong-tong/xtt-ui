95f7ce64811f0cc4ee44419592f19123a7f34beb
小恸恸 <wtt150249@gmail.com>
2023-07-26 16:06:27 +0800

    feat: 更新 numberfield 点击 + - 按钮时的行为

--- components/numberfield/numberfield.js
+++ components/numberfield/numberfield.js
@@ -43,24 +43,59 @@ export class xttNumberFieldElement extends xttInputElement {
 			this.#input.focus();
 		});
 
-		this.#minus.addEventListener("click", () => {
-			let value = this.value;
-			value -= this.step || 1;
+		const spinBtnsMouseEventHandler = (plused) => {
+			const refreshValue = () => {
+				let value = this.value;
 
-			if (value < this.min) {
-				value = this.min;
-			}
-			this.value = value;
+				if (plused) {
+					const max = this.max;
+					if (max !== "" && value >= max) return;
+
+					value += this.step || 1;
+
+					if (max !== "" && value > max) {
+						value = max;
+					}
+				} else {
+					const min = this.min;
+					if (min !== "" && value <= min) return;
+
+					value -= this.step || 1;
+
+					if (min !== "" && value < min) {
+						value = min;
+					}
+				}
+
+				this.value = value;
+				// 在每次刷新值时，触发 input 事件
+				this.dispatchEvent(new InputEvent("input"));
+			};
+
+			refreshValue();
+
+			const timer = setInterval(() => {
+				refreshValue();
+			}, 150);
+
+			// 监听鼠标抬起事件，清除定时器，以及触发 change 事件
+			document.addEventListener(
+				"mouseup",
+				() => {
+					clearInterval(timer);
+					this.dispatchEvent(new Event("change"));
+				},
+				{
+					once: true
+				}
+			);
+		};
+
+		this.#minus.addEventListener("mousedown", () => {
+			spinBtnsMouseEventHandler(false);
 		});
-
-		this.#plus.addEventListener("click", () => {
-			let value = this.value;
-			value += this.step || 1;
-
-			if (value > this.max) {
-				value = this.max;
-			}
-			this.value = value;
+		this.#plus.addEventListener("mousedown", () => {
+			spinBtnsMouseEventHandler(true);
 		});
 	}
 
@@ -75,28 +110,28 @@ export class xttNumberFieldElement extends xttInputElement {
 	}
 
 	get value() {
-		return Number(super.value);
+		return super.value === "" ? "" : Number(super.value);
 	}
 	set value(value) {
 		super.value = value;
 	}
 
 	get min() {
-		return Number(this.#input.min);
+		return this.#input.min === "" ? "" : Number(this.#input.min);
 	}
 	set min(value) {
 		this.#input.min = value;
 	}
 
 	get max() {
-		return Number(this.#input.max);
+		return this.#input.max === "" ? "" : Number(this.#input.max);
 	}
 	set max(value) {
 		this.#input.max = value;
 	}
 
 	get step() {
-		return Number(this.#input.step);
+		return this.#input.step === "" ? "" : Number(this.#input.step);
 	}
 	set step(value) {
 		this.#input.step = value;
