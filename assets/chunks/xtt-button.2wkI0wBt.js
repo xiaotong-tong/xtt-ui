7335e32f7fbc06f46e5218377aacaff06ec6ce25
xiaotong-tong <xiaotong-tong@users.noreply.github.com>
2024-01-24 14:25:57 +0000

    deploy: b0601a7a0c609be0e5b887827618fb4d7e45e86d

--- assets/chunks/xtt-button.2wkI0wBt.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./button.qWWvxE0N.js";import"./form._HBYyMoL.js";import"./base.Wz2wNpMD.js";import"./reflect.QJLKHJUk.js";import"./xtt-ui-utils.Ke2hgjsQ.js";import"./index.esm.fvrJsKlN.js";customElements.define("xtt-button",t);
--- assets/components_sound.md.75bU2fDz.lean.js
+++
@@ -1,7 +0,0 @@
-import{l as i,a3 as e,A as n,o as r,c as p,V as d}from"./chunks/framework.3DIjh9Lj.js";import"./chunks/com.w40geAFS.js";import{G as l}from"./chunks/lil-gui.esm.J1oMuMs2.js";const c=d("",7),h=[c],E=JSON.parse('{"title":"Page Nav 导航","description":"","frontmatter":{},"headers":[],"relativePath":"components/sound.md","filePath":"components/sound.md"}'),_={name:"components/sound.md"},v=Object.assign(_,{setup(u){let t;return i(async()=>{await Promise.all([e(()=>import("./chunks/xtt-icon.LLrk0Clc.js"),__vite__mapDeps([0,1])),e(()=>import("./chunks/xtt-sound.On4EM7xC.js"),__vite__mapDeps([2,1]))]);const s=document.getElementById("operate");t=new l({container:document.querySelector(".operate-wrapper")});const a={src:"https://xtt.moe/sounds/jp/word/猫かぶり.wav"};t.add(a,"src").onChange(o=>{s.src=o})}),n(()=>{t.destroy()}),(s,a)=>(r(),p("div",null,h))}});export{E as __pageData,v as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/chunks/xtt-icon.LLrk0Clc.js","assets/chunks/base.Wz2wNpMD.js","assets/chunks/xtt-sound.On4EM7xC.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
