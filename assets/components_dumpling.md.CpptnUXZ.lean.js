21a131489ca4cb9b309ae6671cfd9d0d994b890d
xiaotong-tong <xiaotong-tong@users.noreply.github.com>
2024-02-25 14:43:52 +0000

    deploy: 556fb4948ef384caa69f3d7f41bf648f280927e0

---
+++ assets/components_dumpling.md.CpptnUXZ.lean.js
@@ -0,0 +1,7 @@
+import{l as a,a3 as t,o as i,c as s,a4 as e}from"./chunks/framework.Kegde9V2.js";import"./chunks/com.l0sNRNKZ.js";const n=e("",4),l=[n],_=JSON.parse('{"title":"Dumpling 团子","description":"","frontmatter":{},"headers":[],"relativePath":"components/dumpling.md","filePath":"components/dumpling.md"}'),o={name:"components/dumpling.md"},m=Object.assign(o,{setup(p){return a(async()=>{await Promise.all([t(()=>import("./chunks/xtt-dumpling.DnlsQQH1.js"),__vite__mapDeps([0,1]))])}),(d,h)=>(i(),s("div",null,l))}});export{_ as __pageData,m as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/chunks/xtt-dumpling.DnlsQQH1.js","assets/chunks/base.C5bbslE4.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
